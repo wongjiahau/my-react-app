2178bce64124b30b212d92856003b61a42b32f58
WJH <hou32hou@gmail.com>
2017-10-06 20:58:43 +0800

    Added playground component : Hello.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import './App.css';
+import Hello from './playground/Hello';
 
 const logo = require('./logo.svg');
 
@@ -8,12 +9,15 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h2>hello guy</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          To get started, edit
+          <code>src/App.tsx</code>
+          and save to reload.
         </p>
+        <Hello name="hey" enthusiasmLevel={8}/>
       </div>
     );
   }
