2e8be353887f972d80804704c794b5496ffc6861
WJH <hou32hou@gmail.com>
2017-10-06 20:17:17 +0800

    Added deploy script, but not sure is it working properly or not

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#refer to the following link on how to update ghpages properly
+#https://stackoverflow.com/questions/24713112/my-github-page-wont-update-its-content
+git push origin :gh-pages
+npm run deploy
